51647cb2936eca0dfe73a55c633dbb7424369a4e
Aryann Dwivedi <aryanndwivedi2403@gmail.com>
2024-08-26 19:24:55 +0530

    test: adding tests for static_controller simulator action

--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -10,7 +10,15 @@ RSpec.describe StaticController, type: :controller do
       get :simulatorvue, params: { path: "/" }
 
       expect(response).to have_http_status(:success)
-      expect(response.body).to include(Rails.public_path.join("simulatorvue", "index.html").read)
+      expect(response.body).to include(Rails.public_path.join("simulatorvue", "v0", "index-cv.html").read)
+    end
+
+    it "renders the simulatorvue template with specified version" do
+      version = "v1"
+      get :simulatorvue, params: { simver: version }
+
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include(Rails.public_path.join("simulatorvue", version, "index-cv.html").read)
     end
   end
 end
